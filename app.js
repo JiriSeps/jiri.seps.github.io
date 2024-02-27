2b7d65ef161bc64e3872198b5fe668787bffe81a
JiriSeps <jiri.seps@tul.cz>
2024-02-27 19:53:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.parallax-bg').each(function() {
+        const image = $(this).data('image');
+        $(this).css('background-image', `url('./${image}')`);
+      });
+    // Initialize the Slick slider
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+    });
+    
+    // Function to handle the parallax effect
+    function handleParallax() {
+        const scrolled = $(window).scrollTop();
+        // Adjust the speed by changing the multiplier (e.g., 0.3 for faster, 0.7 for slower)
+        $('.parallax-bg').css('transform', 'translateY(' + -(scrolled * 0.25) + 'px)');
+    }
+    
+    // Call handleParallax function when the page is loaded
+    handleParallax();
+    
+    // Call handleParallax function when the user scrolls
+    $(window).on('scroll', handleParallax);
+    
+    // Mobile navigation functionality
+    let hamburger = document.querySelector('.hamburger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+    
+    hamburger.addEventListener('click', function(){
+      mobileNav.classList.add('open');  
+    });
+    
+    times.addEventListener('click', function(){
+        mobileNav.classList.remove('open');  
+    });
+});
